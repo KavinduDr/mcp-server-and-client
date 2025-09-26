592fa97da9ff522146bc7a971dbaf5f99771477e
Kavindu Dhananjaya <rmkavindudhananjaya@gmail.com>
2025-09-26 09:27:47 +0530

    Prompts
    * Add resource selection and handling for dynamic URIs in main function
    
    * feat: integrate Google Generative AI and enhance prompt handling
    
    - Added @ai-sdk/google and ai dependencies to package.json
    - Imported necessary modules for Google Generative AI and prompt handling in client.ts
    - Implemented functionality to select and handle prompts from the model context protocol
    - Added user input for prompt arguments and integrated response handling with Google Generative AI

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,10 @@
-import { input, select } from "@inquirer/prompts";
+import "dotenv/config";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { confirm, input, select } from "@inquirer/prompts";
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
-import { Tool } from "@modelcontextprotocol/sdk/types.js";
+import { Prompt, PromptMessage, Tool } from "@modelcontextprotocol/sdk/types.js";
 
 const mcp = new Client(
     {
@@ -16,6 +19,10 @@ const transport = new StdioClientTransport({
     command: "node",
     args: ["build/server.js"],
     stderr: "ignore", // remove the errors and warnings from the server process. In production you might want to log these somewhere
+})
+
+const google = createGoogleGenerativeAI({
+    apiKey: process.env.GEMINI_API_KEY
 })
 
 async function main() {
@@ -74,6 +81,24 @@ async function main() {
                     await handleResource(uri)
                 }
                 break;
+            case "Prompts":
+                const promptName = await select({
+                    message: "Select a prompt",
+                    choices:
+                        prompts.map(prompt => ({
+                            name: prompt.name,
+                            value: prompt.name,
+                            description: prompt.description,
+                        })),
+
+                })
+                const prompt = prompts.find(p => p.name === promptName)
+                if (prompt == null) {
+                    console.error("Prompt not found")
+                } else {
+                    await handlePrompt(prompt)
+                }
+                break;
         }
     }
 }
@@ -118,6 +143,45 @@ async function handleResource(uri: string) {
 
     console.log(JSON.stringify(JSON.parse(res.contents[0].text as string), null, 2)
     ) // type casting because contents is of type unknown and we are stringifying the parsed in data to make it readable
+}
+
+async function handlePrompt(prompt: Prompt) {
+    const args: Record<string, string> = {} // empty arguments to start with
+    for (const arg of prompt.arguments ?? []) {
+        args[arg.name] = await input({
+            message: `Enter value for ${arg.name}:`,
+        })
+    }
+
+    const response = await mcp.getPrompt({
+        name: prompt.name,
+        arguments: args,
+    })
+
+    for (const message of response.messages) {
+        console.log(await handleServerMessagePrompt(message));
+    }
+}
+
+async function handleServerMessagePrompt(message: PromptMessage) {
+    if (message.content.type !== "text") return;
+
+    console.log(message.content.text);
+    const run = await confirm({
+        message: "Would you like to run this prompt",
+        default: true
+    })
+
+    if (!run) {
+        return;
+    }
+
+    const { text } = await generateText({
+        model: google("gemini-2.0-flash"),
+        prompt: message.content.text
+    })
+
+    return text;
 }
 
 main()
